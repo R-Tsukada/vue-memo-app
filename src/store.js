d2b8240b08ceb795c8ddb439498e724845855b73
R-Tsukada <r.tsukada14@gmail.com>
2021-12-20 13:52:41 +0900

    メモ編集に関するコードを可読性の高いコードに変更

--- src/store.js
+++ src/store.js
@@ -11,9 +11,9 @@ export const store = createStore({
     addMemo: (state, memoData) => {
       state.memos.unshift({id: state.memos.length + 1, content: memoData})
     },
-    editMemo: (state, memoData) => {
-      const memo = state.memos.find(memo => memo.id === memoData.id)
-      memo.content = memoData.content
+    editMemo: (state, {id, content}) => {
+      const memo = state.memos.find(memo => memo.id === id)
+      memo.content = content
     },
     deleteMemo: (state, memoData) => {
       const memoSelected = state.memos.filter((memo) => memo.id != memoData.id)
